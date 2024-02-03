b614f8906435d2f5158b9c0fc892046ae4d15d2c
Klim Fominskiy <132931200+KlimFominskiy@users.noreply.github.com>
2024-02-03 20:00:14 +0300

    feat: implemented the file manager.

---
+++ src/handlers/rn.js
@@ -0,0 +1,18 @@
+import { rename } from 'node:fs/promises'
+import { resolve, parse } from 'node:path'
+import displayCurrentDirectory from '../helpers/displayCurrentDirectory.js'
+
+export default async function rn([pathToFile, newFileName]) {
+  try {
+    if (/[\/\\]/g.test(newFileName)) throw new Error('invalid new_file_name')
+
+    pathToFile = resolve(pathToFile)
+    const { dir } = parse(pathToFile)
+    const pathFromFile = resolve(dir, newFileName)
+    await rename(pathToFile, pathFromFile)
+    displayCurrentDirectory()
+  }
+  catch (error) {
+    console.error('Operation failed')
+  }
+}
